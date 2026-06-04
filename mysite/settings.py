4f1d4e17a55c2aeaa384a2a2aaab9e3813e16b15
jimin <dnpsel2737@gmail.com>
2026-06-04 19:26:10 +0900

    feat: 실습12 이미지 블로그

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,3 +118,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media/"
